01337355e9b595f1c7f3c4f90f874005ffe55bcc
Amaan <71920621+amaank404@users.noreply.github.com>
2024-10-07 18:51:11 +0530

    changes

--- static/main.js
+++ static/main.js
@@ -55,7 +55,9 @@ async function changeFieldSpecifierText() {
         for (const c of greetings[i]) {
             curtext += c;
             elem.innerText = curtext;
-            await sleep(100);
+            if (c !== ' ') {
+                await sleep(100);
+            }
         }
         
 
@@ -66,5 +68,18 @@ async function changeFieldSpecifierText() {
     }
 }
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add("sho");
+        } else {
+            entry.target.classList.remove("sho");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hid');
+hiddenElements.forEach((el) => observer.observe(el));
+
 changeFieldSpecifierText();
-console.log("done");
