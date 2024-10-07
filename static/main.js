58e14c6d2ec6056b00ed4ab408e412292297793a
Amaan <71920621+amaank404@users.noreply.github.com>
2024-10-07 20:17:45 +0530

    add themes

--- static/main.js
+++ static/main.js
@@ -3,7 +3,29 @@ var selected = false;
 let navBar = $(".navbar");
 let titleBg = $(".title-background");
 let Jwindow = $(window);
-let scrollUpButton = $('.scroll-up-button')
+let scrollViewButton = $('.scroll-view-button');
+let scrollUpButton = $('.scroll-up-button');
+let themeButton = $('.theme-button');
+
+let theme = readCookie("theme");
+if (theme === null) {
+    createCookie("theme", "dark", 365);
+    theme = "dark";
+}
+setTheme(theme);
+
+function setTheme(theme) {
+    let bodyTag = $('body');
+    if (theme === "dark") {
+        bodyTag.removeClass("light-theme");
+        themeButton.text("dark_mode");
+        createCookie("theme", "dark");
+    } else {
+        bodyTag.addClass("light-theme");
+        themeButton.text("light_mode");
+        createCookie("theme", "light");
+    }
+}
 
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scrolly', window.scrollY);
@@ -19,19 +41,27 @@ window.addEventListener('scroll', () => {
         navBar.removeClass("navbar-borders");
     }
 
-    if (window.scrollY > Jwindow.height()) {
-        scrollUpButton.removeClass("hidden");
+    if (window.scrollY > Jwindow.height()/2) {
+        scrollViewButton.removeClass("hidden");
     } else {
-        scrollUpButton.addClass("hidden");
+        scrollViewButton.addClass("hidden");
     }
 
 }, false);
-
 
 scrollUpButton.on("click", () => {
     $('html, body').animate({
         scrollTop: 0
     }, 800, ()=>{});
+})
+
+themeButton.on("click", () => {
+    let bodyTag = $('body');
+    if (bodyTag.hasClass("light-theme")) {
+        setTheme("dark");
+    } else {
+        setTheme("light");
+    }
 })
 
 function sleep(ms) {
